f2cf54e83bf26578483454141bdf6e6392a8d6ce
Marsel1010 <119671591+Marsel1010@users.noreply.github.com>
2023-01-13 15:32:43 +0300

    lighting approach

--- Assets/MAIN/Script/Conflict.cs
+++ Assets/MAIN/Script/Conflict.cs
@@ -5,11 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class Conflict : MonoBehaviour
 {
+
+    public GameObject light; 
+
 	public void OnTriggerEnter2D(Collider2D collision)
 	{
         if (collision.gameObject.name == "Rock" || collision.gameObject.name == "Rock_2")
         {
             SceneManager.LoadScene("Conflict");
+        }
+        if (collision.gameObject.name == "Alarm")
+        {
+            light.SetActive(true);
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+	{
+               if (collision.gameObject.name == "Alarm")
+        {
+            light.SetActive(false);
         }
     }
 }
